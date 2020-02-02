8b7631b1d3c1126bd32d3730b5d5548b60d20d3f
shadow-chadow <56468853+shadow-chadow@users.noreply.github.com>
2020-02-02 21:28:52 +0530

    replaced class components with hooks

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,66 +1,63 @@
-import React, {PureComponent} from "react"
+import React, {useState, useEffect, createContext} from "react"
 
-const AnimeContext = React.createContext()
+const AnimeContext = createContext()
 const years = ["2014", "2015", "2016", "2017", "2018", "2019", "2020"]
 const cours = ["1", "2", "3", "4", "all"]
 const seasons = ["冬", "春", "夏", "秋"]
-let allAnimes = []
 
-class AnimeProvider extends PureComponent {
-	state = {
-		allAnimes: [],
-		loading: true
-	}
+const AnimeProvider = props => {
+	const [allAnimes, setAllAnimes] = useState([])
+	const [loading, setLoading] = useState(true)
 
-	componentDidMount = async () => {
-		try {
-			for (const year of years) {
-				for (const cour of cours) {
-					const response = await this.getData(year, cour)
-					const animes = await response.json()
-					allAnimes.push({year, cour, animes})
-				}
-			}
-			this.setState({allAnimes, loading: false})
-		} catch (e) {
-			console.error(e)
+	useEffect(() => {
+		const getData = async (year, cour) => {
+			cour = cour === "all" ? "" : `/${cour}`
+			const response = await fetch(
+				`https://api.moemoe.tokyo/anime/v1/master/${year}${cour}`
+			)
+			return response
 		}
+
+		;(async () => {
+			try {
+				let tempAllAnimes = []
+				for (const year of years) {
+					for (const cour of cours) {
+						const response = await getData(year, cour)
+						const animes = await response.json()
+						tempAllAnimes.push({year, cour, animes})
+					}
+				}
+				setAllAnimes(tempAllAnimes)
+				setLoading(false)
+			} catch (e) {
+				console.error(e)
+			}
+		})()
+	}, [])
+
+	const getAnime = (year, cour) => {
+		return allAnimes.find(e => e.year === year && e.cour === cour)
 	}
 
-	getData = async (year, cour) => {
-		cour = cour === "all" ? "" : `/${cour}`
-		const response = await fetch(
-			`https://api.moemoe.tokyo/anime/v1/master/${year}${cour}`
-		)
-		return response
-	}
-
-	getAnime = (year, cour) => {
-		return this.state.allAnimes.find(e => e.year === year && e.cour === cour)
-	}
-
-	getSeason = e => {
+	const getSeason = e => {
 		const season = seasons[e - 1] ? seasons[e - 1] : "全て"
 		return season
 	}
 
-	render() {
-		console.log(this.state.allAnimes)
-
-		return (
-			<AnimeContext.Provider
-				value={{
-					...this.state,
-					years,
-					cours,
-					updateCondition: this.updateCondition,
-					getAnime: this.getAnime,
-					getSeason: this.getSeason
-				}}>
-				{this.props.children}
-			</AnimeContext.Provider>
-		)
-	}
+	return (
+		<AnimeContext.Provider
+			value={{
+				allAnimes,
+				loading,
+				years,
+				cours,
+				getAnime,
+				getSeason
+			}}>
+			{props.children}
+		</AnimeContext.Provider>
+	)
 }
 
 const AnimeConsumer = AnimeProvider.Consumer
